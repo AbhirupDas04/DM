1a5a01bf2f2a8615d8370304cd7a8f7cabcc2f0d
AbhirupDas04 <abhirup22019@iiitd.ac.in>
2023-10-02 02:10:39 +0530

    More Changes

--- TestCase1.py
+++ TestCase1.py
@@ -1,6 +1,11 @@
 WALL = 0
 BOUNDARY = 1
 
+class Graph:
+    def __init__(self,list_vertices,list_edges):
+        self.list_vertices = list_vertices
+        self.list_edges = list_edges
+
 class Vertex:
     def __init__(self,x_coord,y_coord,list_adj_vertices,vertex_type):
         self.x_coord = x_coord
@@ -9,6 +14,7 @@ class Vertex:
         self.vertex_type = vertex_type
 
 class Edge:
-    def __init__(self,vertex_1,vertex_2):
+    def __init__(self,vertex_1,vertex_2,edge_type):
         self.vertex_1 = vertex_1
         self.vertex_2 = vertex_2
+        self.edge_type = edge_type
