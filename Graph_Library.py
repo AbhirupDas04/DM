921290ed13edc4614e22a3060b094d1072319c4d
AbhirupDas04 <abhirup22019@iiitd.ac.in>
2023-10-03 13:55:09 +0530

    Making TestCase1

--- Graph_Library.py
+++ Graph_Library.py
@@ -6,25 +6,25 @@ class Graph:
         self.list_edges = []
         self.max_length = max_length
         self.max_breadth = max_breadth
-        self.list_vertices = [[0 for i in range(max_breadth)] for j in range(max_length)]
+        self.list_vertices = [[0 for i in range(max_breadth+1)] for j in range(max_length+1)]
 
     def addVertice(self,x1,y1,vertex_type):
-        if(self.list_vertices[x1][y1] != 0):
+        if(self.list_vertices[y1][x1] != 0):
             return -1
         else:
-            self.list_vertices[x1][y1] = Vertex(x1,y1,vertex_type)
+            self.list_vertices[y1][x1] = Vertex(y1,x1,vertex_type)
 
     def addEdge(self,x1,y1,x2,y2,edge_type):
-        if(self.list_vertices[x1][y1] == 0 or self.list_vertices[x2][y2] == 0):
+        if(self.list_vertices[y1][x1] == 0 or self.list_vertices[y2][x2] == 0):
             return -1
 
-        if(self.list_vertices[x1][y1] in self.list_vertices[x2][y2].list_adj_vertices):
+        if(self.list_vertices[y1][x1] in self.list_vertices[y2][x2].list_adj_vertices):
             return -1
         
-        edge = Edge(self.list_vertices[x1][y1],self.list_vertices[x2][y2],edge_type)
+        edge = Edge(self.list_vertices[y1][x1],self.list_vertices[y2][x2],edge_type)
         self.list_edges.append(edge)
-        self.list_vertices[x1][y1].list_adj_vertices.append(self.list_vertices[x2][y2])
-        self.list_vertices[x2][y2].list_adj_vertices.append(self.list_vertices[x1][y1])
+        self.list_vertices[y1][x1].list_adj_vertices.append(self.list_vertices[y2][x2])
+        self.list_vertices[y2][x2].list_adj_vertices.append(self.list_vertices[y1][x1])
     
 class Vertex:
     def __init__(self,x_coord,y_coord,vertex_type):
@@ -32,6 +32,12 @@ class Vertex:
         self.y_coord = y_coord
         self.list_adj_vertices = []
         self.vertex_type = vertex_type
+
+    def __str__(self):
+        return f"[{self.y_coord} , {self.x_coord}]"
+    
+    def __repr__(self):
+        return f"[{self.y_coord} , {self.x_coord}]"
 
 class Edge:
     def __init__(self,vertex_1,vertex_2,edge_type):
--- TestCase1.py
+++ TestCase1.py
@@ -1,1 +1,30 @@
 from Graph_Library import *
+
+g1 = Graph(23,24)
+
+for i in range(25):
+    g1.addVertice(i,23,BOUNDARY)
+
+for i in range(10,24):
+    g1.addVertice(0,i,BOUNDARY)
+
+for i in range(1,6):
+    g1.addVertice(i,10,BOUNDARY)
+
+for i in range(11,17):
+    g1.addVertice(5,i,BOUNDARY)
+
+for i in range(16,5,-1):
+    g1.addVertice(6,i,BOUNDARY)
+
+g1.addVertice(5,7,BOUNDARY)
+g1.addVertice(4,8,BOUNDARY)
+g1.addVertice(3,7,BOUNDARY)
+g1.addVertice(2,6,BOUNDARY)
+g1.addVertice(1,6,BOUNDARY)
+
+for i in range(6,2,-1):
+    g1.addVertice(0,i,BOUNDARY)
+
+for i in range(24):
+    print(g1.list_vertices[i])
