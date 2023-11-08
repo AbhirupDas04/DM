781fb0daa54264f27b15688040548a1bf6ec7f0f
Armaan Singh <123837862+aturtle4@users.noreply.github.com>
2023-11-08 16:48:44 +0000

    dindi

--- scratch2.py
+++ scratch2.py
@@ -1,6 +1,4 @@
-from shapely.geometry import Polygon
-import math
-import matplotlib.pyplot as plt
+
 '''
 from shapely.geometry import Polygon
 
@@ -10,7 +8,9 @@ polyb = Polygon([(0.5, 0.5), (0.5, 0.8), (0.8, 0.8), (0.8, 0.5)])
 polya.contains(polyb)
 https://stackoverflow.com/questions/55840924/how-to-judge-if-a-polygon-is-inside-another-polygon-in-python
 '''
-
+from shapely.geometry import Polygon
+import math
+import matplotlib.pyplot as plt
 def calculate_angle(p1, p2, p3):
     vector1 = [p2[0] - p1[0], p2[1] - p1[1]]
     vector2 = [p3[0] - p2[0], p3[1] - p2[1]]
@@ -31,10 +31,11 @@ def calculate_angle(p1, p2, p3):
 def triangulate(dummylist):
     if len(dummylist) < 3:
         print("Insufficient plots")
-        return []
+        return None
+    main_poly = Polygon(dummylist)
     triangles = []
+
     while len(dummylist) > 2:
-        main_poly = Polygon(dummylist)
         for i in range(len(dummylist)):
             prev = dummylist[i - 1]
             curr = dummylist[i]
@@ -44,27 +45,209 @@ def triangulate(dummylist):
                 triangles.append(ear)
                 dummylist.pop(i)
                 break
-    return triangles
 
-def plot_polygon_and_triangles(original_polygon, triangles):
+    for triangle in triangles:
+        main_poly = main_poly.difference(triangle)
+
+    # Extract vertices from each triangle and store them in a list
+    triangle_vertices = []
+    for triangle in triangles:
+        triangle_vertices.append(list(triangle.exterior.coords))
+
+    return main_poly, triangle_vertices
+
+def plot_polygon_and_triangles(main_polygon, triangle_vertices):
     fig, ax = plt.subplots()
 
-    # Plot the original polygon
-    x, y = original_polygon.exterior.xy
+    # Plot the main polygon
+    x, y = main_polygon.exterior.xy
     ax.fill(x, y, alpha=0.5, color='blue', edgecolor='black')
 
-    # Plot the triangulated triangles
-    for triangle in triangles:
-        x, y = triangle.exterior.xy
+    # Plot the triangles as a list of vertices
+    for vertices in triangle_vertices:
+        x, y = zip(*vertices)  # Separate x and y coordinates
         ax.fill(x, y, alpha=0.5, color='green', edgecolor='black')
 
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.title('Original Polygon and Triangles')
+    plt.title('Main Polygon with Triangles (List of Vertices)')
     plt.show()
 
-dummylist = [[10, -78], [12, -17], [80, -23], [78, -88], [64, -87], [67, -37], [30, -32], [28, -44], [43, -48], [43, -84], [10, -79]]
-p = Polygon(dummylist)
-triangles = triangulate(dummylist)
 
-plot_polygon_and_triangles(p, triangles)
+def count_guards(triangle_vertices):
+    a=[]
+    b=[]
+    c=[]
+    
+    for i in triangle_vertices: 
+        x = i[0]
+        y = i[1]
+        z = i[2]
+        
+        if x in a or x in b or x in c:
+            pass
+        else:
+            if y in a:
+                if z in b:
+                    c.append(x)
+                elif z in c:
+                    b.append(x)
+                else:
+                    b.append(x)
+            elif y in b:
+                if z in a:
+                    c.append(x)
+                elif z in c:
+                    a.append(x)
+                else:
+                    a.append(x)
+            elif y in c:
+                if z in b:
+                    a.append(x)
+                elif z in a:
+                    b.append(x)
+                else:
+                    a.append(x)
+            elif z in a:
+                if y in b:
+                    c.append(x)
+                elif y in c:
+                    b.append(x)
+                else:
+                    b.append(x)
+            elif z in b:
+                if y in a:
+                    c.append(x)
+                elif y in c:
+                    a.append(x)
+                else:
+                    a.append(x)
+            elif z in c:
+                if y in b:
+                    a.append(x)
+                elif y in a:
+                    b.append(x)
+                else:
+                    a.append(x)
+            else:
+                a.append(x)
+        
+        if y in a or y in b or y in c:
+            pass
+        else:
+            if x in a:
+                if z in b:
+                    c.append(y)
+                elif z in c:
+                    b.append(y)
+                else:
+                    b.append(y)
+            elif x in b:
+                if z in a:
+                    c.append(y)
+                elif z in c:
+                    a.append(y)
+                else:
+                    a.append(y)
+            elif x in c:
+                if z in b:
+                    a.append(y)
+                elif z in a:
+                    b.append(y)
+                else:
+                    a.append(y)
+            elif z in a:
+                if x in b:
+                    c.append(y)
+                elif x in c:
+                    b.append(y)
+                else:
+                    b.append(y)
+            elif z in b:
+                if x in a:
+                    c.append(y)
+                elif x in c:
+                    a.append(y)
+                else:
+                    a.append(y)
+            elif z in c:
+                if x in b:
+                    a.append(y)
+                elif x in a:
+                    b.append(y)
+                else:
+                    a.append(y)
+            else:
+                a.append(y)        
+        
+        if z in a or z in b or z in c:
+            pass
+        else:
+            if y in a:
+                if x in b:
+                    c.append(z)
+                elif x in c:
+                    b.append(z)
+                else:
+                    b.append(z)
+            elif y in b:
+                if x in a:
+                    c.append(z)
+                elif x in c:
+                    a.append(z)
+                else:
+                    a.append(z)
+            elif y in c:
+                if x in b:
+                    a.append(z)
+                elif x in a:
+                    b.append(z)
+                else:
+                    a.append(z)
+            elif x in a:
+                if y in b:
+                    c.append(z)
+                elif y in c:
+                    b.append(z)
+                else:
+                    b.append(z)
+            elif x in b:
+                if y in a:
+                    c.append(z)
+                elif y in c:
+                    a.append(z)
+                else:
+                    a.append(z)
+            elif x in c:
+                if y in b:
+                    a.append(z)
+                elif y in a:
+                    b.append(z)
+                else:
+                    a.append(z)
+            else:
+                a.append(z)            
+    
+    guards =[]
+    guards.append(len(a))
+    guards.append(len(b))
+    guards.append(len(c))
+    min_guards = min(guards)
+    if min_guards == len(a):
+        return a
+    elif min_guards == len(b):
+        return b
+    elif min_guards == len(c):
+        return c
+    
+
+
+
+dummylist = [[21,-83],[9,-71],[8 , -59],[20 , -73],[5 , -47],[12 , -47],[24 , -56],[4 , -25],[36 , -14],[72 , -31],[71 , -58],[71 , -77],[83 , -84],[82 , -89],[68 , -83],[64 , -92],[51 , -93],[48 , -83],[45 , -93],[51 , -96],[28 , -95],[19 , -92]]
+print(len(dummylist))
+main_polygon, triangle_vertices = triangulate(dummylist)
+print(triangle_vertices)
+print(count_guards(triangle_vertices))
+plot_polygon_and_triangles(main_polygon, triangle_vertices)
+
+
